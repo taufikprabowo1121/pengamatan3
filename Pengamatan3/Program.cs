8b887388fcad4d12f1fc4b32a58f223667b0966e
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 16:22:48 +0700

    pengamatan3

---
+++ Pengamatan3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pengamatan3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tester t = new Tester();
+            //output with params
+            t.DisplayVals(5, 6, 7, 8);
+            //output without params
+            int[] explicitArray = new int[5] { 1, 2, 3, 4, 5};
+            t.DisplayVals(explicitArray);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Pengamatan3/Tester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pengamatan3
+{
+    class Tester
+    {
+        public void DisplayVals(params int[] intVals)
+        {
+            foreach (int i in intVals)
+            {
+                Console.WriteLine("DisplayVals {0}", i);
+            }
+        }
+    }
+}
